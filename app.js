6b2f1f84cd13dce9c23dadc6e1666c17c605a33b
aidanliddiard <aidanliddiard@gmail.com>
2021-12-08 11:18:17 -0800

    formating edits

--- app.js
+++ app.js
@@ -2,18 +2,18 @@ const audio = document.getElementById('audio');
 
 const rainSound = document.getElementById('rain-sound');
 rainSound.addEventListener('click', () => {
-    audio.src = 'audio/rain.wav';
-    audio.play();
-})
+  audio.src = 'audio/rain.wav';
+  audio.play();
+});
 
 const oceanSound = document.getElementById('ocean-sound');
-oceanSound.addEventListener('click', ( => {
-    audio.src = 'audio/ocean.wav';
-    audio.play();
-}))
+oceanSound.addEventListener('click', () => {
+  audio.src = 'audio/ocean.wav';
+  audio.play();
+});
 
 const fanSound = document.getElementById('fan-sound');
-fanSound.addEventListener('click', ( => {
-    audio.src = 'audio/fan.wav';
-    audio.play();
-}))
+fanSound.addEventListener('click', () => {
+  audio.src = 'audio/fan.wav';
+  audio.play();
+});
