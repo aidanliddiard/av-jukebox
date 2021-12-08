65a491dd933d150c99461dbb26fa1d3aa81960f8
aidanliddiard <aidanliddiard@gmail.com>
2021-12-08 15:24:49 -0800

    js edits

--- app.js
+++ app.js
@@ -4,25 +4,19 @@ const template = document.getElementById('template');
 const soundList = document.getElementById('sound-list');
 const audio = document.getElementById('audio');
 
-// loop the array
+
 for (let sleepSound of sleepSounds) {
-  // for each sound:
-  // make a copy of the template
   const copy = template.content.cloneNode(true);
-  // find the h2, img, and button elements
   const name = copy.querySelector('h2');
   const image = copy.querySelector('img');
-  const button = copy.querySelector('button');
-  // set the h2.textContent and img.src
+  const button = copy. querySelector('button');
   name.textContent = sleepSound.name;
   image.src = sleepSound.image;
 
-  // subscribe to button click
   button.addEventListener('click', () => {
     audio.src = sleepSound.audio;
     audio.play();
   });
 
-  // add (append) the copy to the list (ul)
   soundList.appendChild(copy);
 }
