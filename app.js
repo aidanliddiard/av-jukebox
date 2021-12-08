65d41fcf9d6afe4b6104e413bf042628301d948b
aidanliddiard <aidanliddiard@gmail.com>
2021-12-08 11:16:13 -0800

    audio and js

---
+++ app.js
@@ -0,0 +1,19 @@
+const audio = document.getElementById('audio');
+
+const rainSound = document.getElementById('rain-sound');
+rainSound.addEventListener('click', () => {
+    audio.src = 'audio/rain.wav';
+    audio.play();
+})
+
+const oceanSound = document.getElementById('ocean-sound');
+oceanSound.addEventListener('click', ( => {
+    audio.src = 'audio/ocean.wav';
+    audio.play();
+}))
+
+const fanSound = document.getElementById('fan-sound');
+fanSound.addEventListener('click', ( => {
+    audio.src = 'audio/fan.wav';
+    audio.play();
+}))
